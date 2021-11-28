3e92bc5f6bd4d3682a29fb0623a9b0c390ae95a8
FreyCT <freydenit@gmail.com>
2021-11-28 15:40:56 -0800

    styles todo

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,10 @@
 import React, {useState, useEffect} from "react";
+/*
+need to apply style sheets and add a background to helloworld.
+Hello world needs to be in the center of the screen and font size increased
+every time the onclick is called, a random color is assigned as the background
+*/
+
 
 export default function App(){
 
